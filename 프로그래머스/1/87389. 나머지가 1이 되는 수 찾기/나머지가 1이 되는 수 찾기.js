b61b5e88bfb90779dd767aa3e1de89f3c6986a77
Dohyun Lee <120698922+00-Dohyun-00@users.noreply.github.com>
2025-06-28 00:55:45 +0900

    [level 1] Title: 나머지가 1이 되는 수 찾기, Time: 5.26 ms, Memory: 36.5 MB -BaekjoonHub

---
+++ 프로그래머스/1/87389. 나머지가 1이 되는 수 찾기/나머지가 1이 되는 수 찾기.js
@@ -0,0 +1,29 @@
+function solution(n) {
+    for(let i = 0; i<n; i++){
+        if(n % i === 1){
+            return i
+        }
+    }
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    // for(let i=1; i<=n; i++){
+    //     if(n%i===1)return i
+    // }
+}
