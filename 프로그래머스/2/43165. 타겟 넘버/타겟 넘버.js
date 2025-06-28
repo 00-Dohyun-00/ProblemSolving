190cacfb0fb066189e35e163989c43e4d0203915
Dohyun Lee <120698922+00-Dohyun-00@users.noreply.github.com>
2025-06-28 18:19:53 +0900

    [level 2] Title: 타겟 넘버, Time: 14.64 ms, Memory: 35.5 MB -BaekjoonHub

--- 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.js
+++ 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.js
@@ -1,19 +1,21 @@
 function solution(numbers, target) {
     var answer = 0;
     
-    const dfs = (idx, sum) => {
+    const dfs = (index, sum) => {
+      
         // 탈출
-        if(idx === numbers.length){
-            if(target === sum){
+        if(index === numbers.length){
+            if(sum === target){
                 answer++;
             }
             return;
         }
-            
+        
         // 진행
-        dfs(idx+1, sum + numbers[idx]);
-        dfs(idx+1, sum - numbers[idx]);
+        dfs(index+1, sum + numbers[index]);
+        dfs(index+1, sum - numbers[index]);
     }
+    
     dfs(0,0);
     return answer;
 }
