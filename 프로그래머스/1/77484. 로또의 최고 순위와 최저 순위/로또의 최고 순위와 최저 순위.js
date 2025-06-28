03bc920b0d4dfd9830c39c3abfeee42e7e6ed1a8
Dohyun Lee <120698922+00-Dohyun-00@users.noreply.github.com>
2025-06-28 17:54:31 +0900

    [level 1] Title: 로또의 최고 순위와 최저 순위, Time: 0.07 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/1/77484. 로또의 최고 순위와 최저 순위/로또의 최고 순위와 최저 순위.js
+++ 프로그래머스/1/77484. 로또의 최고 순위와 최저 순위/로또의 최고 순위와 최저 순위.js
@@ -1,12 +1,13 @@
 function solution(lottos, win_nums) {
     var answer = [];
-    const RANKING = [6,6,5,4,3,2,1]
-    
+    let rank = [6,6,5,4,3,2,1];
     let matched = lottos.filter(item => win_nums.includes(item)).length;
-    let zeros = lottos.filter(item => item === 0).length;
+    let zero = lottos.filter(item => item === 0).length;
     
-    let max = RANKING[matched + zeros];
-    let min = RANKING[matched];
+    
+    let max = rank[matched + zero];
+    let min = rank[matched];
+    
     answer = [max, min];
     return answer;
 }
