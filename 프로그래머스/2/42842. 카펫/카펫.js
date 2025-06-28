8bb279e6e56ad5027a93cc7f90f1abc249aea02a
Dohyun Lee <120698922+00-Dohyun-00@users.noreply.github.com>
2025-06-28 18:42:44 +0900

    [level 2] Title: 카펫, Time: 0.19 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/2/42842. 카펫/카펫.js
+++ 프로그래머스/2/42842. 카펫/카펫.js
@@ -1,21 +1,14 @@
 function solution(brown, yellow) {
     var answer = [];
-
-        let pfm = []; // prime factorization math
-        // 소인수분해
-        for(let i = 1; i <= yellow; i++){
-            if(yellow % i === 0) pfm.push(i);
-        }
-        // 갈색 개수 찾기
-        for(let i = 0; i < pfm.length / 2; i++){
-            let brownCnt = ((pfm[i] + 2) * 2) + (pfm[pfm.length - 1 - i] * 2);
-            if(brownCnt === brown){
-                let width = pfm[pfm.length - 1 - i] + 2;
-                let length = pfm[i] + 2;
-                answer = [width, length];
-            }
-        }  
-
-        
+    
+    for(let i=1; i<=yellow; i++){
+        let x = yellow / i;
+        let y = i
+        let need = ((x + 2) * 2) + (i * 2); 
+        if(need === brown){
+            answer = [(x + 2),  (i + 2)];
+            break;
+        }   
+    }
     return answer;
 }
