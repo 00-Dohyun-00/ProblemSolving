8ad36cbad79fdd48c225203a462ed752c5c62888
Dohyun Lee <120698922+00-Dohyun-00@users.noreply.github.com>
2025-06-28 18:49:04 +0900

    [level 1] Title: K번째수, Time: 0.08 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/1/42748. K번째수/K번째수.js
+++ 프로그래머스/1/42748. K번째수/K번째수.js
@@ -1,33 +1,12 @@
 function solution(array, commands) {
     var answer = [];
     
-    // 20250627
-    commands.forEach((item) => {
-        let newarr = JSON.parse(JSON.stringify(array)); 
-        newarr = newarr.splice(item[0]-1, item[1] - item[0] +1) ;
-        newarr.sort((a,b) => a - b);
-        answer.push(newarr[item[2] -1]);
+    commands.forEach(item => {
+        let newarr = array.slice(item[0]-1, item[1]).sort((a,b)=> a-b);
+        answer.push(newarr[item[2]-1]);
     })
     
     
     
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-
-//     for(let i =0; i<commands.length; i++){
-//         answer.push(array.slice(commands[i][0]-1,commands[i][1]).sort((a,b) => a - b)[commands[i][2]-1]);
-        
-//     }
-   
     return answer;
 }
