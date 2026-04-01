0b5540192f6b687a6d7c3e995bafe0670971ea45
Dohyun Lee <120698922+00-Dohyun-00@users.noreply.github.com>
2026-04-01 21:22:12 +0900

    [level 0] Title: 첫 번째로 나오는 음수, Time: 0.01 ms, Memory: 4.14 MB -BaekjoonHub

---
+++ 프로그래머스/0/181896. 첫 번째로 나오는 음수/첫 번째로 나오는 음수.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
+
+// num_list_len은 배열 num_list의 길이입니다.
+int solution(int num_list[], size_t num_list_len) {
+    int answer = 0;
+    int i = 0;
+    while(num_list[i] >= 0){
+        if(i == num_list_len){
+            i = -1;
+            break;
+        }
+        i++;
+    }
+    answer = i;
+    return answer;
+}
