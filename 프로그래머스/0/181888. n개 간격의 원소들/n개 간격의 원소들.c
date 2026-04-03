8b4028504d32cae1bead53a329c993bd94bd3bad
Dohyun Lee <120698922+00-Dohyun-00@users.noreply.github.com>
2026-04-03 20:24:47 +0900

    Update n개 간격의 원소들.c

--- 프로그래머스/0/181888. n개 간격의 원소들/n개 간격의 원소들.c
+++ 프로그래머스/0/181888. n개 간격의 원소들/n개 간격의 원소들.c
@@ -5,16 +5,23 @@
 // num_list_len은 배열 num_list의 길이입니다.
 int* solution(int num_list[], size_t num_list_len, int n) {
     // return 값은 malloc 등 동적 할당을 사용해주세요. 할당 길이는 상황에 맞게 변경해주세요.
-    int* answer = (int*)malloc(20 * sizeof(int));
+    int size = 0;
     int idx = 0;
     
-    for(int i = 0; i<num_list_len; i++){
-        if(num_list_len - 1 < idx){
-            break;
-        }
+    while(idx < num_list_len){
+        idx += n;
+        size++;
+    }
+    
+    int* answer = (int*)malloc(size * sizeof(int));
+    int i = 0;
+    idx = 0;
+    
+    while(idx < num_list_len){
         answer[i] = num_list[idx];
         idx += n;
-        
+        i++;
     }
+    
     return answer;
 }
