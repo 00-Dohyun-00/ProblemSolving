404edd76c543d86a6cc3b123b3e1e1d75431bdce
Dohyun Lee <120698922+00-Dohyun-00@users.noreply.github.com>
2026-04-04 19:02:26 +0900

    [level 0] Title: 배열의 원소 삭제하기, Time: 0.05 ms, Memory: 4.15 MB -BaekjoonHub

---
+++ 프로그래머스/0/181844. 배열의 원소 삭제하기/배열의 원소 삭제하기.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
+
+// arr_len은 배열 arr의 길이입니다.
+// delete_list_len은 배열 delete_list의 길이입니다.
+int* solution(int arr[], size_t arr_len, int delete_list[], size_t delete_list_len) {
+    // return 값은 malloc 등 동적 할당을 사용해주세요. 할당 길이는 상황에 맞게 변경해주세요.
+    int* answer = (int*)malloc(arr_len * sizeof(int));
+    int idx = 0;
+    
+    for(int i = 0; i < arr_len; i++){
+        bool is_found = false;
+        for(int j = 0; j < delete_list_len; j++){
+            if(arr[i] == delete_list[j]){
+                is_found = true;
+                break;
+            }
+        }
+        if(!is_found){
+            answer[idx] = arr[i];
+            idx++;
+        }  
+        
+    }
+    return answer;
+}
