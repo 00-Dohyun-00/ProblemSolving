b36b9f2372611a82f8dae09a6c1f4a61b91fd0dc
Dohyun Lee <120698922+00-Dohyun-00@users.noreply.github.com>
2026-04-04 11:34:53 +0900

    Update 문자열로 변환.c

--- 프로그래머스/0/181845. 문자열로 변환/문자열로 변환.c
+++ 프로그래머스/0/181845. 문자열로 변환/문자열로 변환.c
@@ -4,14 +4,7 @@
 
 char* solution(int n) {
     // return 값은 malloc 등 동적 할당을 사용해주세요. 할당 길이는 상황에 맞게 변경해주세요.
-    int size = 0;
-    int copy_n = n;
-    while(copy_n >= 10){
-        copy_n /= 10;
-        size++;
-    }
-    char* answer = (char*)malloc(size * sizeof(char));
-    
+    char* answer = (char*)malloc(6);
     
     sprintf(answer, "%d", n);
     
