03f32ec4b83e6864c63a1f462ada6318d5a85be8
Dohyun Lee <120698922+00-Dohyun-00@users.noreply.github.com>
2026-04-04 11:12:14 +0900

    [level 0] Title: 문자열로 변환, Time: 0.01 ms, Memory: 4.21 MB -BaekjoonHub

---
+++ 프로그래머스/0/181845. 문자열로 변환/문자열로 변환.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
+
+char* solution(int n) {
+    // return 값은 malloc 등 동적 할당을 사용해주세요. 할당 길이는 상황에 맞게 변경해주세요.
+    int size = 0;
+    int copy_n = n;
+    while(copy_n >= 10){
+        copy_n /= 10;
+        size++;
+    }
+    char* answer = (char*)malloc(size * sizeof(char));
+    
+    
+    sprintf(answer, "%d", n);
+    
+    return answer;
+}
