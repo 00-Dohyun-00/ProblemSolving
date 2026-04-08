8420f9f95d46115f3df1d24598d3735f9db41596
Dohyun Lee <120698922+00-Dohyun-00@users.noreply.github.com>
2026-04-08 16:42:55 +0900

    [level 0] Title: 주사위 게임 1, Time: 0.01 ms, Memory: 4.46 MB -BaekjoonHub

---
+++ 프로그래머스/0/181839. 주사위 게임 1/주사위 게임 1.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
+
+int solution(int a, int b) {
+    int answer = 0;
+    
+    if(a % 2 != 0 && b % 2 != 0){
+        answer = a*a + b*b;
+    } else if((a % 2 != 0 && b % 2 == 0) || (b % 2 != 0 && a % 2 == 0)){
+        answer = 2 * (a + b);
+    } else if(a % 2 == 0 && b % 2 == 0){
+        if(a - b < 0){
+          answer = -(a - b);
+        } else {
+          answer = a - b;
+        }
+    }
+    return answer;
+}
