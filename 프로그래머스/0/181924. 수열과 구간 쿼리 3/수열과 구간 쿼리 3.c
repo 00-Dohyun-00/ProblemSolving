cfd39c8d69646c1620b45cdce743c775884b1769
Dohyun Lee <120698922+00-Dohyun-00@users.noreply.github.com>
2026-04-09 16:17:02 +0900

    [level 0] Title: 수열과 구간 쿼리 3, Time: 0.87 ms, Memory: 3.88 MB -BaekjoonHub

---
+++ 프로그래머스/0/181924. 수열과 구간 쿼리 3/수열과 구간 쿼리 3.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
+
+// arr_len은 배열 arr의 길이입니다.
+// queries_rows는 2차원 배열 queries의 행 길이, queries_cols는 2차원 배열 queries의 열 길이입니다.
+int* solution(int arr[], size_t arr_len, int** queries, size_t queries_rows, size_t queries_cols) {
+    // return 값은 malloc 등 동적 할당을 사용해주세요. 할당 길이는 상황에 맞게 변경해주세요.
+
+    for(int i = 0; i < queries_rows; i++){
+        int num0 = 0;
+        int idx0 = 0;
+        int num1 = 0;
+        int idx1 = 1;
+        for(int j = 0; j < arr_len; j++){
+            if(j == queries[i][0]){
+                num0 = arr[j];
+                idx0 = j;
+            } 
+            if(j == queries[i][1]){
+                num1 = arr[j];
+                idx1 = j;
+            } 
+        }
+        
+        arr[idx0] = num1;
+        arr[idx1] = num0;
+        
+    }
+    
+    return arr;
+}
