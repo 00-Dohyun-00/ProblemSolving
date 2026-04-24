fccc4ec9e3bdd0f4dba4cbfbbaa4acc72f61cb38
Dohyun Lee <120698922+00-Dohyun-00@users.noreply.github.com>
2026-04-24 21:48:09 +0900

    [level 2] Title: 프로세스, Time: 3.67 ms, Memory: 35.6 MB -BaekjoonHub

--- 프로그래머스/2/42587. 프로세스/프로세스.js
+++ 프로그래머스/2/42587. 프로세스/프로세스.js
@@ -1,19 +1,44 @@
 function solution(priorities, location) {
-    var answer = 0; 
-    let newarr = []; 
-    priorities.map((v, i) => newarr.push({'key': v, 'idx':i}));
+    var answer = 0;
+    let nowidx = location;
+    let bigger = false;
     
-    while(newarr.length > 0){
-        let keys = newarr.map(v => v.key);
-        if(newarr[0].key === Math.max(...keys)){
-            answer++;   
-            if(newarr[0].idx === location) break;
-            newarr.splice(0, 1);        
+    while(true){
+        
+        let arr = priorities.filter((item, idx) => {
+            if(idx !== 0){
+                return item > priorities[0];
+            }
+        });
+        if(arr.length !== 0){
+            bigger = true;
         } else {
-            newarr.push(newarr[0]);
-            newarr.splice(0, 1);        
+            bigger = false;
         }
+        
+                
+        if(bigger){
+            priorities.push(priorities[0])
+            priorities.shift();
+            if(nowidx == 0){
+                nowidx = priorities.length - 1;
+            } else {
+                nowidx--;
+            }
+        } else {
+            if(nowidx == 0){
+                answer++;
+                 console.log('sfwf')
+                break;
+            } else {
+                nowidx--;
+            }
+            priorities.shift();
+            answer++;
+        }
+        
+        
+
     }
-    
     return answer;
 }
