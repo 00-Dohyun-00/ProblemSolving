77833a17f0f246627ba89896ce559c800fbee41b
Dohyun Lee <120698922+00-Dohyun-00@users.noreply.github.com>
2026-04-24 20:57:45 +0900

    [level 2] Title: 기능개발, Time: 0.18 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/2/42586. 기능개발/기능개발.js
+++ 프로그래머스/2/42586. 기능개발/기능개발.js
@@ -1,22 +1,28 @@
 function solution(progresses, speeds) {
     var answer = [];
-    let q = [];
+    let time = [];
     
-    for(let i =0; i<progresses.length; i++){
-        q.push(Math.ceil((100 - progresses[i]) / speeds[i]));
+    for(let i = 0; i<progresses.length; i++){
+        time.push(Math.ceil((100 - progresses[i]) / speeds[i]));
     }
-    
-    let check = q[0];
-    let cnt = 1;
-    for(let i=1; i<q.length + 1; i++){
-        if(check >= q[i]){
+
+    let cnt = 0;
+    let idx = 0;
+    for(let i = 0; i<time.length; i++){
+        if(i == 0){
             cnt++;
-        } else {
-            check = q[i];
-            answer.push(cnt);
-            cnt = 1;
+        }else {
+            if(time[i] > time[idx]){
+                answer.push(cnt);
+                cnt = 1;
+                idx = i;
+            } else {
+                cnt++;
+            }
         }
     }
+    answer.push(cnt);
+    
     
     return answer;
 }
