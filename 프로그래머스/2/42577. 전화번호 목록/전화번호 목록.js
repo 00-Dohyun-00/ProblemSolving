55cc551d4af5d96773d8e817a59953e4b4c8f2ba
Dohyun Lee <120698922+00-Dohyun-00@users.noreply.github.com>
2026-04-25 22:49:05 +0900

    [level 2] Title: 전화번호 목록, Time: 169.21 ms, Memory: 80.2 MB -BaekjoonHub

--- 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.js
+++ 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.js
@@ -3,14 +3,12 @@ function solution(phone_book) {
     
     phone_book.sort();
     
-    for(let i=0; i<phone_book.length-1; i++){
-     
-        // 접두어면 answer = false;
-        if(phone_book[i+1].startsWith(phone_book[i])){
+    for(let i = 0; i<phone_book.length -1; i++){
+        if(phone_book[i + 1].startsWith(phone_book[i])){
             answer = false;
-            break;
         }
- 
     }
+
+    
     return answer;
 }
