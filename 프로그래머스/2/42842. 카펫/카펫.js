13f91394ff48bc19d46d9e23038983b558c5c9be
Dohyun Lee <120698922+00-Dohyun-00@users.noreply.github.com>
2026-04-25 23:07:06 +0900

    [level 2] Title: 카펫, Time: 0.14 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/2/42842. 카펫/카펫.js
+++ 프로그래머스/2/42842. 카펫/카펫.js
@@ -1,14 +1,14 @@
 function solution(brown, yellow) {
     var answer = [];
     
-    for(let i=1; i<=yellow; i++){
-        let x = yellow / i;
-        let y = i
-        let need = ((x + 2) * 2) + (i * 2); 
-        if(need === brown){
-            answer = [(x + 2),  (i + 2)];
-            break;
-        }   
+    for(let i = 1; i <= yellow; i++){
+        if(yellow % i == 0){
+            if((((yellow / i) * 2) + (i * 2) + 4) == brown){
+                answer = [((yellow / i) + 2) , (i + 2)];
+                break;
+            }
+        }
     }
+    
     return answer;
 }
