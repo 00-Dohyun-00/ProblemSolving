82a488add4c035bad10096975035da4ba7affe0f
Dohyun Lee <120698922+00-Dohyun-00@users.noreply.github.com>
2026-04-27 16:45:04 +0900

    [level 2] Title: 짝지어 제거하기, Time: 67.68 ms, Memory: 55.7 MB -BaekjoonHub

--- 프로그래머스/2/12973. 짝지어 제거하기/짝지어 제거하기.js
+++ 프로그래머스/2/12973. 짝지어 제거하기/짝지어 제거하기.js
@@ -1,48 +1,28 @@
 function solution(s)
 {
-    // 20250627
+    var answer = -1;
+    s = s.split('');
     let stack = [];
     
     for(let i = 0; i<s.length; i++){
-        if(stack.length === 0 || stack[stack.length -1] !== s[i]){
+        if(i == 0){
             stack.push(s[i]);
+        } else if(s[i] == stack[stack.length -1]){
+            stack.pop();
         } else {
-            stack.splice(stack.length -1, 1);
+            stack.push(s[i]);
         }
+    
     }
-
-    if(stack.length === 0){
-        return 1
+    
+    if(stack.length == 0){
+        return 1;
     } else {
-        return 0
-    }   
-        
-// 20231118  
-//     let aStack =[]
-//     let bStack =[]
+        return 0;
+    }
     
-//     s = s.split('')
+
     
-//     const f = (stack1, stack2, s, i) => {
-//          if(s[i] === stack1[stack1.length-1]){
-//                 stack1.pop()
-//             } else {
-//                 stack2.push(s[i])
-//             }
-//     }
-    
-//     for(let i = 0 ; i < s.length; i++){
-//         if(i % 2 !== 0){
-//             f(bStack, aStack, s, i)
-//         } else if(i % 2 === 0){
-//             f(aStack, bStack, s, i)
-//         }
-//     }
-    
-//     if(aStack.length === 0 && bStack.length === 0){
-//         return 1
-//     } else {
-//         return 0
-//     }
- 
+
+    return answer;
 }
