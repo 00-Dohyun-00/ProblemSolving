7f69c7e107d03e81ca76de46d9a7be8fa2e0ec3e
Dohyun Lee <120698922+00-Dohyun-00@users.noreply.github.com>
2026-04-28 15:40:39 +0900

    [level 2] Title: 영어 끝말잇기, Time: 0.33 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/2/12981. 영어 끝말잇기/영어 끝말잇기.js
+++ 프로그래머스/2/12981. 영어 끝말잇기/영어 끝말잇기.js
@@ -1,54 +1,41 @@
 function solution(n, words) {
     var answer = [];
-    let wrongIdx = -1;
+    let is = false;
+    let idx = -1;
 
-    for(let i = 0; i<words.length ; i++){
-        // 한 글자인지
-        if(words[i].split('').length == 1){
-            wrongIdx = i;
+    for(let i = 0; i<words.length; i++){
+        // 마지막 문자 확인
+        if(i != 0 && words[i-1][words[i-1].length-1] !== words[i][0]){
+            is = true;
+            idx = i;
+            break;
         }
-        
-        //1. 이어지는 단어인지
-        if(i !== 0){
-            let f = words[i].split('')[0];
-            let l = words[i-1].split('')[words[i-1].length - 1];
-            if(f !== l){
-                if((wrongIdx != -1 && i < wrongIdx) || wrongIdx == -1){
-                    wrongIdx = i;
-                }
-            }
-        }
-        
-        //2. 같은 단어 쓴 적 있는지
-        for(let j = 0; j<i; j++){
+            
+        // 등장했던 단어인지 확인
+        for(let j = 0; j < i; j++){
             if(words[i] == words[j]){
-                if((wrongIdx != -1 && i < wrongIdx) || wrongIdx == -1){
-                    wrongIdx = i;
-                }
+                is = true;
+                idx = i;
+                break;
             }
         }
-    }
-    
-    // 없으면
-    if(wrongIdx == -1){
-        return [0,0];
-    }
-    
-    // 있으면
-    let a = 0;
-    let b = 1;
-    for(let i = 0; i<wrongIdx+1; i++){
-        if(a == n){
-            a = 1;
-            b++;
-        } else {
-            a++;
+        if(is) break;
+            
+        // 한 글자인지 확인
+        if(words[i].length == 1){
+            is = true;
+            idx = i;
+            break;
         }
     }
     
-    answer = [a, b];
+    if(!is) return [0,0];
     
+ let cnt = (idx % n) + 1;
+let round = Math.floor(idx / n) + 1;
+
     
+    answer = [cnt, round]
 
     return answer;
 }
