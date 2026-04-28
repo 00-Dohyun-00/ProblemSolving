3d843543b6bf6afaf6a27fadaa27b152cd84d3b3
Dohyun Lee <120698922+00-Dohyun-00@users.noreply.github.com>
2026-04-28 13:23:04 +0900

    [level 2] Title: 짝지어 제거하기, Time: 68.86 ms, Memory: 59.8 MB -BaekjoonHub

--- 프로그래머스/2/12973. 짝지어 제거하기/짝지어 제거하기.js
+++ 프로그래머스/2/12973. 짝지어 제거하기/짝지어 제거하기.js
@@ -1,28 +1,27 @@
 function solution(s)
 {
     var answer = -1;
-    s = s.split('');
     let stack = [];
-    
+    s = s.split('');
+
     for(let i = 0; i<s.length; i++){
         if(i == 0){
             stack.push(s[i]);
-        } else if(s[i] == stack[stack.length -1]){
-            stack.pop();
         } else {
-            stack.push(s[i]);
+            if(stack[stack.length-1] == s[i]){
+                stack.pop();
+            } else {
+                stack.push(s[i]);
+            }
         }
-    
     }
     
     if(stack.length == 0){
-        return 1;
-    } else {
-        return 0;
+        answer = 1;
+    }else {
+        answer = 0;
     }
-    
-
-    
+   
 
     return answer;
 }
